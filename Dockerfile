55bbd94bad0d7b0bc7d3d270516e2a9471697ad7
Komal <Komal04.sahani@gmail.com>
2026-06-24 20:57:54 +0000

    Add Docker and GitHub Actions files

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM ubuntu
+
+RUN echo "Hello from my first Docker image"
+
+CMD ["echo", "I am learning Docker for DevOps"]
---
+++ system-info.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "system Information"
+echo "------------------"
+
+echo "user:"
+whoami
+
+echo "current folder:"
+pwd
+
+echo "Date:"
+date
